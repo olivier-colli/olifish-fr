d2221b2781af8575754228d33830e5fa1300311c
pntbr <stephane.langlois@scopyleft.fr>
2018-12-28 17:56:11 +0100

    use templating to separate js and html

--- cli/composeIndex.mjs
+++ cli/composeIndex.mjs
@@ -9,7 +9,6 @@ import fs from 'fs'
 import path from 'path'
 import {promisify} from 'util'
 import slugify from './slugify.mjs'
-import readConfig from './readConfig.mjs'
 
 const tplDir = './cli/template'
 
@@ -18,6 +17,7 @@ const tplHead = path.join(tplDir, 'tpl-head.html')
 const tplCss = path.join(tplDir, 'tpl-style.css')
 const tplJS = path.join(tplDir, 'tpl-script.js')
 const tplBody = path.join(tplDir, 'tpl-body.html')
+const tplGalleries = path.join(tplDir, 'tpl-galleries.html')
 
 const readFile = promisify(fs.readFile)
 
@@ -26,12 +26,14 @@ const readIndexFiles = Promise.all([
   readFile(tplHead, 'utf-8').then(head => ({head: head})),
   readFile(tplCss, 'utf-8').then(css => ({css: css})),
   readFile(tplJS, 'utf-8').then(js => ({js: js})),
-  readFile(tplBody, 'utf-8').then(body => ({body: body}))
+  readFile(tplBody, 'utf-8').then(body => ({body: body})),
+  readFile(tplGalleries, 'utf-8').then(galleries => ({galleries: galleries}))
 ])
 
 readIndexFiles.then(tplsData => {
-  const {configGalleries, head, js, css, body} = Object.assign({}, ...tplsData)
-  const bodyWithGalleries = body.replace('${galleries}', composeGalleries(configGalleries))
+  const {configGalleries, head, js, css, body, galleries} = Object.assign({}, ...tplsData)
+  const htmlGalleries = composeGalleries(configGalleries, galleries)
+  const bodyWithGalleries = nano(body, {galleries: htmlGalleries})
   const index = `
     ${head}
     <style>${css}</style>
@@ -47,18 +49,25 @@ const writeHome = content =>
     if (err) throw err
   })
 
-const composeGalleries = config =>
-  config.map(galleryData => composeGallery(galleryData)).join('')
+const composeGalleries = (config, tpl) =>
+  config.map(galleryData => composeGallery(galleryData, tpl)).join('')
 
-const composeGallery = data => {
-  const {title, description, img} = data
-  return `
-      <figure class="img">
-        <a href="/galleries/${slugify(title)}.html" class="gallery">
-          <img src="https://www.olifish.com/light-thumbs/thumb-${img}.jpg" alt="${title}">
-        </a>
-        <figcaption itemprop="caption description">${description}</figcaption>
-        <h3>${title}</h3>
-      </figure>
-  `
+const composeGallery = (data, tpl) => {
+  const datas = {
+    title: data.title,
+    description: data.description,
+    img: data.img,
+    href: `/galleries/${slugify(data.title)}.html`,
+    src: `https://www.olifish.com/light-thumbs/thumb-${data.img}.jpg`
+  }
+  return nano(tpl, datas)
+}
+
+/* Nano Templates - https://github.com/trix/nano */
+function nano(template, data) {
+  return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+    var keys = key.split("."), v = data[keys.shift()]
+    for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]]
+    return (typeof v !== "undefined" && v !== null) ? v : ""
+  })
 }
