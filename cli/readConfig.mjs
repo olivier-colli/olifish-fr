c30c3b454cfc8d2cfb08af0b548ee8932caeac3d
pntbr <stephane.langlois@scopyleft.fr>
2018-12-28 17:55:12 +0100

    delete unused readconfig file

--- cli/readConfig.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-export default callback =>
-  fs.readFile('config.json', 'utf8', (err, json) => {
-    if (err) throw err
-    const config = JSON.parse(json)
-    callback(config)
-  })
