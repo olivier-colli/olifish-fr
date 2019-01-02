1e62b038ce102c909b05c504f9731f04659774cf
pntbr <stephane.langlois@scopyleft.fr>
2019-01-02 21:56:20 +0100

    test within git add

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -14,7 +14,4 @@ git checkout --orphan gh-pages
 npm run compose-index
 npm run compose-galleries
 
-git add --force index.html $galleries
-git commit -m "add galleries and index"
-
 git push --force https://$token@github.com/olivier-colli/olifish-fr.git
