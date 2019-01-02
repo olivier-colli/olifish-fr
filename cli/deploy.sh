4dcd68bc5c9e45bbe856e1902c18462efbbacea0
pntbr <stephane.langlois@scopyleft.fr>
2019-01-02 15:23:16 +0100

    remove old galleries folder

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/env bash
 
-npm run compose-index
-npm run compose-galleries
+galleries='./galeries'
 
 if [[ "$TRAVIS_COMMIT_MESSAGE" == "Travis: cpmopse index and galleries" ]]
 then
   exit 0
 fi
+
+# init
+rm -Rf $galleries
+mkdir $galeries
+
+# compose index and galleries page
+npm run compose-index
+npm run compose-galleries
