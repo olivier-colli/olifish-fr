6686ebb518b499b09ad2b66cd93de5fbb6a30f12
pntbr <pntbr@pm.me>
2020-01-19 13:43:42 +0100

    adds prettierrc

--- cli/db.mjs
+++ cli/db.mjs
@@ -1,17 +1,21 @@
-import axios from "axios";
-import { slugify } from "./utils.mjs";
-import path_ from "./path.mjs";
+import axios from 'axios'
+import { slugify } from './utils.mjs'
+import path_ from './path.mjs'
 
 /*
     Allow to load and find photo's metas in DB file 
 */
 
 export default {
-  load: () => axios.get(path_.dbUrl.href),
-  findAll: (db, query) =>
-    db
-      .filter(
-        metas => slugify(JSON.stringify(metas)).indexOf(slugify(query)) !== -1
-      )
-      .sort((a, b) => (a.nameFr > b.nameFr ? 1 : b.nameFr > a.nameFr ? -1 : 0))
-};
+    load: () => axios.get(path_.dbUrl.href),
+    findAll: (db, query) =>
+        db
+            .filter(
+                metas =>
+                    slugify(JSON.stringify(metas)).indexOf(slugify(query)) !==
+                    -1
+            )
+            .sort((a, b) =>
+                a.nameFr > b.nameFr ? 1 : b.nameFr > a.nameFr ? -1 : 0
+            )
+}
--- cli/nano.mjs
+++ cli/nano.mjs
@@ -1,9 +1,14 @@
-/* Nano Templates - https://github.com/trix/nano */
+/* 
+    Nano Templates - https://github.com/trix/nano
+    Allow to inject data in html template pages    
+*/
+
 export default function nano(template, data) {
     return template.replace(/\{([\w.]*)\}/g, function(str, key) {
         const keys = key.split('.')
         let v = data[keys.shift()]
         for (let i = 0, l = keys.length; i < l; i++) v = v[keys[i]]
-        return (typeof v !== 'undefined' && v !== null) ? v : ''
+
+        return typeof v !== 'undefined' && v !== null ? v : ''
     })
 }
