7dc64874461f9f00e9fce68ecfca458ed161e610
pntbr <pntbr@pm.me>
2020-01-20 14:24:51 +0100

    inserts galleries dir dynamicaly

--- cli/composeIndex.mjs
+++ cli/composeIndex.mjs
@@ -6,37 +6,49 @@
 */
 
 import fs from 'fs'
-import {promisify} from 'util'
+import { promisify } from 'util'
 import yaml from 'js-yaml'
-import {slugify, keysValueArraysToMap} from './utils.mjs'
+import { slugify, keysValueArraysToMap } from './utils.mjs'
 import path_ from './path.mjs'
 import nano from './nano.mjs'
 
 const readFile = promisify(fs.readFile)
 
 const readIndexFiles = Promise.all([
-    readFile(path_.configGalleries, 'utf-8').then(configGalleries => (['configGalleries', yaml.load(configGalleries)])),
-    readFile(path_.tplHead, 'utf-8').then(head => (['head', head])),
-    readFile(path_.tplCss, 'utf-8').then(css => (['css', css])),
-    readFile(path_.tplIndexCss, 'utf-8').then(css => (['indexCss', css])),
-    readFile(path_.tplJS, 'utf-8').then(js => (['js', js])),
-    readFile(path_.tplHeader, 'utf-8').then(header => (['header', header])),
-    readFile(path_.tplIndex, 'utf-8').then(body => (['body', body])),
-    readFile(path_.tplGalleries, 'utf-8').then(galleries => (['galleries', galleries]))
+    readFile(path_.configGalleries, 'utf-8').then(configGalleries => [
+        'configGalleries',
+        yaml.load(configGalleries)
+    ]),
+    readFile(path_.tplHead, 'utf-8').then(head => ['head', head]),
+    readFile(path_.tplCss, 'utf-8').then(css => ['css', css]),
+    readFile(path_.tplIndexCss, 'utf-8').then(css => ['indexCss', css]),
+    readFile(path_.tplJS, 'utf-8').then(js => ['js', js]),
+    readFile(path_.tplHeader, 'utf-8').then(header => ['header', header]),
+    readFile(path_.tplIndex, 'utf-8').then(body => ['body', body]),
+    readFile(path_.tplGalleries, 'utf-8').then(galleries => [
+        'galleries',
+        galleries
+    ])
 ])
 
 readIndexFiles.then(promisesResult => {
     const file = keysValueArraysToMap(promisesResult)
 
-    const htmlGalleries = composeGalleries(file.get('configGalleries'), file.get('galleries'))
-    const body = nano(file.get('body'), {galleries: htmlGalleries})
+    const htmlGalleries = composeGalleries(
+        file.get('configGalleries'),
+        file.get('galleries')
+    )
+    const body = nano(file.get('body'), { galleries: htmlGalleries })
+    const js = nano(file.get('js'), { galleriesDir: path_.galleriesDir })
     const index = `
         ${file.get('head')}
         <style>
             ${file.get('css')}
-            ${nano(file.get('indexCss'), {'backgroundImage': './assets/background-image.jpg'})}
+            ${nano(file.get('indexCss'), {
+                backgroundImage: './assets/background-image.jpg'
+            })}
         </style>
-        <script>${file.get('js')}</script>
+        <script>${js}</script>
         ${file.get('header')}
         ${body}
     </html>
