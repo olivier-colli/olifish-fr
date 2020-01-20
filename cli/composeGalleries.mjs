b8e4e1eecf9e28d897688b029ea25f2ea3682263
pntbr <pntbr@pm.me>
2020-01-20 12:31:25 +0100

    updates README

--- cli/composeGalleries.mjs
+++ cli/composeGalleries.mjs
@@ -5,10 +5,10 @@
 
 import fs from 'fs'
 import path from 'path'
-import {promisify} from 'util'
+import { promisify } from 'util'
 import yaml from 'js-yaml'
 import db from './db.mjs'
-import {slugify, keysValueArraysToMap} from './utils.mjs'
+import { slugify, keysValueArraysToMap } from './utils.mjs'
 import path_ from './path.mjs'
 import nano from './nano.mjs'
 
@@ -16,22 +16,28 @@ const readFile = promisify(fs.readFile)
 
 const readIndexFiles = Promise.all([
     db.load().then(db => ['db', db.data]),
-    readFile(path_.configGalleries, 'utf-8').then(configGalleries => (['configGalleries', yaml.load(configGalleries)])),
-    readFile(path_.tplHead, 'utf-8').then(head => (['head', head])),
-    readFile(path_.tplCss, 'utf-8').then(css => (['css', css])),
-    readFile(path_.tplJS, 'utf-8').then(js => (['js', js])),
-    readFile(path_.tplPhotoswipe, 'utf-8').then(photoswipe => (['photoswipe', photoswipe])),
-    readFile(path_.tplHeader, 'utf-8').then(header => (['header', header])),
-    readFile(path_.tplGallery, 'utf-8').then(body => (['body', body])),
-    readFile(path_.tplThumb, 'utf-8').then(thumb => (['thumb', thumb])),
+    readFile(path_.configGalleries, 'utf-8').then(configGalleries => [
+        'configGalleries',
+        yaml.load(configGalleries)
+    ]),
+    readFile(path_.tplHead, 'utf-8').then(head => ['head', head]),
+    readFile(path_.tplCss, 'utf-8').then(css => ['css', css]),
+    readFile(path_.tplJS, 'utf-8').then(js => ['js', js]),
+    readFile(path_.tplPhotoswipe, 'utf-8').then(photoswipe => [
+        'photoswipe',
+        photoswipe
+    ]),
+    readFile(path_.tplHeader, 'utf-8').then(header => ['header', header]),
+    readFile(path_.tplGallery, 'utf-8').then(body => ['body', body]),
+    readFile(path_.tplThumb, 'utf-8').then(thumb => ['thumb', thumb])
 ])
 
 readIndexFiles.then(promisesResult => {
     const file = keysValueArraysToMap(promisesResult)
     file.get('configGalleries').map(galleryMeta => {
-        const photosMetas = db.findAll( file.get('db'), galleryMeta.title)
+        const photosMetas = db.findAll(file.get('db'), galleryMeta.title)
         const gallery = composeGallery(galleryMeta, photosMetas, file)
-        const body = nano(file.get('body'), {gallery: gallery})
+        const body = nano(file.get('body'), { gallery: gallery })
         const galleryContent = `
             ${file.get('head')}
             <style>${file.get('css')}</style>
@@ -50,7 +56,7 @@ function composeGallery(galleryMetas, photosMetas, file) {
     let htmlGallery = '<div class=fish>'
     let counter = 0
     for (const meta of photosMetas) {
-        let metas = {   
+        let metas = {
             fishname: meta.nameFr,
             fishnameLatin: meta.nameLat,
             imgUrl: `${path_.photosRepoUrl}/${meta.filepath.img}`,
@@ -59,7 +65,7 @@ function composeGallery(galleryMetas, photosMetas, file) {
             imgSize: `${meta.imgSize.width}x${meta.imgSize.height}`,
             thumbSizeWidth: meta.thumbSize.width,
             thumbSizeHeight: meta.thumbSize.height,
-            title: meta.location,
+            title: meta.location
         }
         htmlGallery += nano(file.get('thumb'), metas)
     }
--- cli/template/tpl-script.js
+++ cli/template/tpl-script.js
@@ -1,5 +1,7 @@
 window.dataLayer = window.dataLayer || []
-function gtag(){dataLayer.push(arguments)}
+function gtag() {
+    dataLayer.push(arguments)
+}
 gtag('js', new Date())
 gtag('config', 'UA-108901544-1')
 
@@ -7,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.onsubmit = event => {
         event.preventDefault()
         const query = document.querySelector('input').value
-        document.location.href = `/galleries/#${slugify(query)}`
+        document.location.href = `/galeries/#${slugify(query)}`
     }
 })
 
@@ -16,10 +18,11 @@ const slugify = text => {
     const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------'
     const p = new RegExp(a.split('').join('|'), 'g')
 
-    return text.toString().toLowerCase()
+    return text
+        .toString()
+        .toLowerCase()
         .replace(/\s+/g, '-') // Replace spaces with -
-        .replace(p, c =>
-            b.charAt(a.indexOf(c))) // Replace special chars
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special chars
         .replace(/&/g, '-and-') // Replace & with 'and'
         .replace(/[^\w-]+/g, '') // Remove all non-word chars
         .replace(/--+/g, '-') // Replace multiple - with single -
